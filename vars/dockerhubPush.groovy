2c04d29da6c2638e05ccf6a393d30c3b15d2f64b
sadofrazer <83238471+sadofrazer@users.noreply.github.com>
2021-10-18 02:40:59 +0200

    edit

--- vars/dockerhubPush.groovy
+++ vars/dockerhubPush.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call (){
+def call ( String buildresult){
   sh 'docker login -u ${USERNAME} -p ${PASSWORD}'
   sh 'docker push ${IMAGE_NAME}:${IMAGE_TAG}'
   slackSend color: "danger", message: "BAD NEWS:Job ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its result was unclear ! more info ${env.BUILD_URL}"
