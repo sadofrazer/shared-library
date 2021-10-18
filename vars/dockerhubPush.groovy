266a40a4e544578eae0db0b1606d0229efcc9192
sadofrazer <83238471+sadofrazer@users.noreply.github.com>
2021-10-18 02:11:38 +0200

    edit

--- vars/dockerhubPush.groovy
+++ vars/dockerhubPush.groovy
@@ -1,10 +1,7 @@
 #!/usr/bin/env groovy
-
-def call(String IMAGE_NAME, String IMAGE_TAG, String USERNAME, String PASSWORD) {
     script{
       sh'''
          docker login -u ${USERNAME} -p ${PASSWORD}
          docker push ${IMAGE_NAME}:${IMAGE_TAG}
       '''
     }
-}
